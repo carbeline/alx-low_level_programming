c1af5d4c0ab97663c2d7c37a21029c5ccf940233
carbeline <attakpacornelis@gmail.com>
2023-07-13 11:40:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line
+ *
+ * return:0
+ */
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
