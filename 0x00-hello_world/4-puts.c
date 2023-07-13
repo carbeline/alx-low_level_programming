d2a55b0a4de8c770e85ef9b916282b871708eb7e
carbeline <attakpacornelis@gmail.com>
2023-07-13 11:57:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- *main - prints exactly "Programming is like building a multilingual puzzle"
+ *main - prints exactly "Programming is like building a multilingual puzzle
  *
  * return:Always 0 (Success)
  */
